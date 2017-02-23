b359dbc1ea0d97eecc041e753b20c994fee60c0a
Heekei <heekei@foxmail.com>
2017-02-23 15:09:07 +0800

    new

---
+++ app/scripts/app.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name blogOnAngularJsApp
+ * @description
+ * # blogOnAngularJsApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('blogOnAngularJsApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngSanitize',
+    'ngTouch'
+  ]);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name blogOnAngularJsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the blogOnAngularJsApp
+ */
+angular.module('blogOnAngularJsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
